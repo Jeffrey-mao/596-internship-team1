f145c84c2f5107cc508876df96d3c628f0b28a49
Jeffrey-mao <60928487+jeffrey-mao@users.noreply.github.com>
2023-08-14 17:33:16 +1200

    Start a project

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <el-button type="success">11</el-button>
+    <RadioSelect></RadioSelect>
+  </div>
+</template>
+aaaaaaaa
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import RadioSelect from "./components/radioSelect.vue";
+
+export default {
+  name: "App",
+  components: {
+    RadioSelect,
+  },
+  data() {
+    return {
+      radio: 1,
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/radioSelect.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="question">ques:{{ question }}</div>
+      <div class="top">Select One:</div>
+      <div class="option">
+        <el-radio-group v-model="radio">
+          <div>
+            <el-radio label="a">a. {{ aOption }}</el-radio>
+          </div>
+          <div>
+            <el-radio label="b">b. {{ bOption }}</el-radio>
+          </div>
+          <div>
+            <el-radio label="c">c. {{ cOption }}</el-radio>
+          </div>
+          <div>
+            <el-radio label="d">d. {{ dOption }}</el-radio>
+          </div>
+        </el-radio-group>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RadioSelect",
+  props: ["question", "aOption", "bOption", "cOption", "dOption"],
+  data() {
+    return {
+      radio: 0,
+    };
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.container {
+  margin: 0 auto;
+  border: 1px solid black;
+  width: 50%;
+  background-color: #bee5eb;
+  text-align: left;
+  padding: 20px;
+  .question {
+    font-weight: bolder;
+    color: #052025;
+    line-height: 1.5;
+    font-size: 16px;
+    margin-bottom: 24px;
+  }
+  .top {
+    font-style: 16px;
+    font-weight: 400;
+    line-height: 1.5;
+  }
+  .option div {
+    margin-bottom: 8px;
+    margin-left: 5px;
+  }
+}
+</style>
