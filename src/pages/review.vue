c75bb05ba3b9b2ac1ee83ea7f0a599b85948fc09
ym248 <ym248@students.waikato.ac.nz>
2023-09-19 09:37:11 +1200

    Css of review page

--- src/pages/review.vue
+++ src/pages/review.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <h1>review</h1>
-    <div v-for="(item, index) in $store.state.home.timeArr" :key="index">
+    <div
+        class="review"
+        v-for="(item, index) in $store.state.home.timeArr"
+        :key="index"
+    >
       <span>Jeffrey</span>
       <span>{{ item }}</span>
       <span>{{ $store.state.home.methodsArr[index] }}</span>
@@ -19,5 +23,18 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+h1 {
+  line-height: 50px;
+  text-align: center;
+}
+.review {
+  width: 100%;
+  line-height: 30px;
+  border: 1px solid black;
+  padding-left: 60px;
+}
+.review:nth-child(even) {
+  background-color: greenyellow;
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from "vue-router";
 import Vue from "vue";
 Vue.use(VueRouter);
 
+
 import routes from './routes'
 
 // import store from '@/store'
